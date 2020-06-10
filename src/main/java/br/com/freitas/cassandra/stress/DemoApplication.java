588ab798a48aa182c7427ca516db9a2c8d3bcc69
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2020-06-10 15:58:07 -0300

    Teste

--- src/main/java/br/com/freitas/cassandra/stress/DemoApplication.java
+++ src/main/java/br/com/freitas/cassandra/stress/DemoApplication.java
@@ -3,6 +3,7 @@ package br.com.freitas.cassandra.stress;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class DemoApplication {
 
